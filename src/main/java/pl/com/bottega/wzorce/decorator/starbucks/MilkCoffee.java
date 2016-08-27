d30dad88e0a4ef1ac6fee163c6657f28b5f9f76d
ksrubka <beata.choluj@wp.pl>
2016-08-27 14:21:39 +0200

    Create decorator starbucks app

---
+++ src/main/java/pl/com/bottega/wzorce/decorator/starbucks/MilkCoffee.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.wzorce.decorator.starbucks;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by maciuch on 21.08.16.
+ */
+public class MilkCoffee extends CoffeeDecorator {
+
+    MilkCoffee(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public BigDecimal cost() {
+        return coffee.cost().add(new BigDecimal(1.0));
+    }
+
+    @Override
+    public String name() {
+        return coffee.name() + " with milk";
+    }
+}
---
+++ src/main/java/pl/com/bottega/wzorce/decorator/starbucks/StarbucksPointOfSale.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.wzorce.decorator.starbucks;
+
+/**
+ * Created by maciuch on 21.08.16.
+ */
+public class StarbucksPointOfSale {
+
+    public static void main(String[] args) {
+        Coffee coffee1 = new MilkCoffee(new PeachSyrupCoffee(new SmallCoffee()));
+        Coffee coffee2 = new PeachSyrupCoffee(new LargeCoffee());
+        Coffee coffee3 = new LargeCoffee();
+        System.out.println(coffee1.name() + " - " + coffee1.cost());
+        System.out.println(coffee2.name() + " - " + coffee2.cost());
+        System.out.println(coffee3.name() + " - " + coffee3.cost());
+    }
+}
