e8255469f477d2e12d25186c619f5c47b3b83d8f
ksrubka <beata.choluj@wp.pl>
2016-08-28 16:11:04 +0200

    Start creating vending machine app

---
+++ src/main/java/pl/com/bottega/wzorce/state/vending_machine/VendingMachine.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.wzorce.state.vending_machine;
+
+/**
+ * Created by Beata Iłowiecka on 28.08.2016.
+ */
+public class VendingMachine {
+
+    private VendingMachineState state;
+}
