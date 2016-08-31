0afb1e38b85865adde6884af61496e7ddc6657f7
ksrubka <beata.choluj@wp.pl>
2016-08-31 12:30:48 +0200

    Neaten

--- src/main/java/pl/com/bottega/wzorce/singleton/scrabble/ScrabbleTestThreads.java
+++ src/main/java/pl/com/bottega/wzorce/singleton/scrabble/ScrabbleTestThreads.java
@@ -3,19 +3,14 @@ package pl.com.bottega.wzorce.singleton.scrabble;
 public class ScrabbleTestThreads{
 
     public static void main(String[] args){
-
         // Create a new Thread created using the Runnable interface
         // Execute the code in run after 10 seconds
 
         Runnable getTiles = new GetTheTiles();
-
         Runnable getTilesAgain = new GetTheTiles();
 
         // Call for the code in the method run to execute
-
         new Thread(getTiles).start();
         new Thread(getTilesAgain).start();
-
     }
-
 }
